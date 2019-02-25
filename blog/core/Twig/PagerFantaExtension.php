a5af6685f35dc281a0aee7580e00bb170fa7e741
CyberCreative <mikael.malvache@mgmail.fr>
2019-02-26 00:22:05 +0100

    Update PagerFantaExtension.php

--- blog/core/Twig/PagerFantaExtension.php
+++ blog/core/Twig/PagerFantaExtension.php
@@ -18,7 +18,7 @@ class PagerFantaExtension extends \Twig_Extension
     public function paginate(Pagerfanta $paginatedResults, string $route): string
     {
         $view = new TwitterBootstrap4View();
-        $options = array('css_container_class' => 'pagination justify-content-center');
+        $options = array('css_container_class' => 'pagination justify-content-center', 'prev_message' => 'Précédent', 'next_message' => 'Suivant');
         $routeGenerator = function($page) use ($route) {
         return 'index.php?p='.$route.'&d='.$page;};
         return $view->render($paginatedResults, $routeGenerator, $options);
